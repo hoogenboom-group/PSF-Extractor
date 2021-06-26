3b25bd66c5ba004e901a79b78ab35d16b7e8d114
Ryan Lane <ryanilane@gmail.com>
2021-06-26 16:24:24 +0200

    add bboxes_overlap
    Former-commit-id: 834bc17bb415c4c83ed208ec3205fad412b19624

--- psf_extractor/extractor.py
+++ psf_extractor/extractor.py
@@ -89,3 +89,32 @@ def get_stack(file_pattern):
     # Return stack
     logging.info(f"{stack.shape} image stack created succesfully.")
     return stack
+
+
+def bboxes_overlap(bbox_1, bbox_2):
+    """Determines if two bounding boxes overlap or coincide
+
+    Parameters
+    ----------
+    bbox_1 : 4-tuple
+        1st bounding box
+        convention: (x_min, x_max, y_min, y_max)
+    bbox_2 : 4-tuple
+        2nd bounding box
+        convention: (x_min, x_max, y_min, y_max)
+
+    Returns
+    -------
+    overlap : bool
+        True if bounding boxes overlap / coincide
+        False otherwise
+
+    References
+    ----------
+    [1] https://stackoverflow.com/a/20925869/5285918
+    """
+    # 2 tiles overlap iff their projections onto both x and y axis overlap
+    # Overlap in 1D iff box1_max > box2_min AND box1_min < box2_max
+    overlap = ((bbox_1[2] >= bbox_2[0]) & (bbox_1[0] <= bbox_2[2])) & \
+              ((bbox_1[3] >= bbox_2[1]) & (bbox_1[1] <= bbox_2[3]))
+    return overlap
