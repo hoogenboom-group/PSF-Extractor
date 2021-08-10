50217b3783321212c5a6b6291fcca11df710f98c
Ryan Lane <ryanilane@gmail.com>
2021-08-11 01:10:57 +0200

    Add a bunch of plotting functionality
    Former-commit-id: 10df8b9ae0b6edd310be9f78308b98158b05bf72

--- psf_extractor/plotting.py
+++ psf_extractor/plotting.py
@@ -1,13 +1,241 @@
+from tqdm.notebook import tqdm
+import numpy as np
+import trackpy
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+from .extractor import get_mip, crop_psf
+from .gauss import gaussian_1D, fit_gaussian_1D
+from .util import get_Daans_special_cmap
 
 
-__all__ = ['plot_psf',
+__all__ = ['plot_mip',
+           'plot_minmasses_',
+           'plot_features',
+           'plot_psf',
+           'plot_psfs',
            'plot_psf_localizations']
 
 
-def plot_psf():
-    pass
+# No-brainer global variable
+fire = get_Daans_special_cmap()
+
+
+def plot_mip(stack, axis=0):
+    """Plot maximum intensity projection"""
+    # Get maximum intensity projection
+    mip = get_mip(stack, axis=axis, log=True)
+    # Create figure
+    fig, ax = plt.subplots(figsize=(7, 7))
+    im = ax.imshow(mip, cmap=fire)
+    ax.set_title('Maximum intensity projection of image stack')
+    # Colorbar
+    div = make_axes_locatable(ax)
+    cax = div.append_axes('right', size='5%', pad=0.05)
+    plt.colorbar(im, cax=cax)
+
+
+def plot_features(stack, dx, dy, axis=0, min_mass=None):
+    """Detect features from maximum intensity projection and overlay them
+
+    Parameters
+    ----------
+    stack : array-like
+        3D image stack
+    dx, dy : int
+        Diameters in x and y
+    min_mass : scalar
+        Minimum mass that gets passed to `trackpy.locate`.
+        Minimum mass refers to the minimum integrated brightness, which
+        is apparently "a crucial parameter for eliminating spurious
+        features. See trackpy documentation for more details.
+        Defaults to 500
+    """
+    # Set minimum mass if not provided
+    min_mass = 500
+    # Get maximum intensity projection
+    mip = get_mip(stack, axis=axis)
+
+    # Round diameters up to nearest odd integer (as per `trackpy` instructions)
+    dx = int(np.ceil(dx)//2*2 + 1)
+    dy = int(np.ceil(dy)//2*2 + 1)
+    # Locate features
+    df_features = trackpy.locate(mip,
+                                 diameter=(dx, dy),
+                                 minmass=min_mass).reset_index(drop=True)
+
+    # Create figure
+    fig, ax = plt.subplots(figsize=(7, 7))
+    # Take log to enhance contrast (avoiding /b0 error)
+    mip_log = np.log(mip,
+                     out=np.zeros_like(mip),
+                     where=mip!=0)
+    # Plot max projection image
+    ax.imshow(mip_log, cmap=fire)
+    # Plot detected features
+    ax.plot(df_features['x'], df_features['y'], ls='', color='#00ff00',
+            marker='o', ms=15, mfc='none', mew=1)
+    title = f'Min Mass: {min_mass} | Features Detected: {len(df_features):.0f}'
+    ax.set_title(title)
+
+
+def plot_minmasses_(stack, dy, dx, axis=0, min_masses=None):
+    """Plot detected features from MIP for a range of minimum masses
+
+    Parameters
+    ----------
+    stack : array-like
+        3D image stack
+    dx, dy : int
+        Diameters in x and y
+    min_masses : list
+        Set of candidate masses that gets passed to `trackpy.locate`.
+        Minimum mass refers to the minimum integrated brightness, which
+        is apparently "a crucial parameter for eliminating spurious
+        features. See trackpy documentation for more details.
+        Defaults to [50, 100, 200, 500, 1000, 2000]
+    """
+    # Round diameters up to nearest odd integer (as per `trackpy` instructions)
+    dx = int(np.ceil(dx)//2*2 + 1)
+    dy = int(np.ceil(dy)//2*2 + 1)
+
+    # Set candidate minimum masses if not provided
+    if min_masses is None:
+        min_masses = [50, 100, 200, 500, 1000, 2000]
+
+    # Set up figure
+    ncols = 3
+    nrows = int(np.ceil(len(min_masses) / ncols))
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols,
+                             figsize=(6*ncols, 6*nrows))
+
+    # Loop through candidate minimum masses
+    for i, min_mass in tqdm(enumerate(min_masses),
+                            total=len(min_masses)):
+
+        # Get maximum intensity projection in given axis
+        mip = get_mip(stack, axis=axis)
+
+        # Locate features
+        df_features = trackpy.locate(mip,
+                                     diameter=(dy, dx), # expects row, col order
+                                     minmass=min_mass).reset_index(drop=True)
+
+        # Plot max projection image
+        ax = axes.flat[i]
+        # Take log to enhance contrast (avoiding /b0 error)
+        mip_log = np.log(mip,
+                         out=np.zeros_like(mip),
+                         where=mip!=0)
+        ax.imshow(mip_log, cmap=fire)
+        # Plot detected features
+        ax.plot(df_features['x'], df_features['y'], ls='', color='#00ff00',
+                marker='o', ms=15, mfc='none', mew=1)
+        title = f'Min Mass: {min_mass} | Features Detected: {len(df_features):.0f}'
+        ax.set_title(title)
+
+
+def plot_psf(psf, psx, psy, psz, crop=True):
+    """"""
+
+    # Create figure and axes
+    fig = plt.figure(figsize=(11, 11))
+    gs = fig.add_gridspec(6, 6)
+    ax_xy = fig.add_subplot(gs[:3,:3])
+    ax_yz = fig.add_subplot(gs[:3,3:])
+    ax_xz = fig.add_subplot(gs[3:,:3])
+    ax_z = fig.add_subplot(gs[-3,3:])
+    ax_y = fig.add_subplot(gs[-2,3:])
+    ax_x = fig.add_subplot(gs[-1,3:])
+
+    # Crippity crop crop
+    if crop:
+        psf = crop_psf(psf)
+
+    # PSF dimensions
+    Nz, Ny, Nx = psf.shape
+    # PSF volume
+    dz, dy, dx = psz*Nz/1e3, psy*Ny/1e3, psx*Nx/1e3
+    # PSF center coords
+    z0, y0, x0 = Nz//2, Ny//2, Nx//2
+
+    # Plot 2D PSFs (slices)
+    ax_xy.imshow(psf[z0,:,:], extent=[-dx/2, dx/2, -dy/2, dy/2], cmap=fire)
+    ax_yz.imshow(psf[:,y0,:], extent=[-dz/2, dz/2, -dy/2, dy/2], cmap=fire)
+    ax_xz.imshow(psf[:,:,x0], extent=[-dx/2, dx/2, -dz/2, dz/2], cmap=fire)
+    # 1D PSFs (slices)
+    prof_z = psf[:,y0,x0]
+    prof_y = psf[z0,:,x0]
+    prof_x = psf[z0,y0,:]
+    # Do 1D PSF fits
+    u = np.linspace(0, Nz, 10*Nz)
+    popt_z = fit_gaussian_1D(prof_z)
+    popt_y = fit_gaussian_1D(prof_y)
+    popt_x = fit_gaussian_1D(prof_x)
+    # Scale axes
+    x_prof_z = np.linspace(-dz/2, dz/2, prof_z.size)
+    x_prof_y = np.linspace(-dy/2, dy/2, prof_y.size)
+    x_prof_x = np.linspace(-dx/2, dx/2, prof_x.size)
+    x_uz = np.linspace(-dz/2, dz/2, u.size)
+    x_uy = np.linspace(-dy/2, dy/2, u.size)
+    x_ux = np.linspace(-dx/2, dx/2, u.size)
+
+    # Plot 1D PSFs
+    plot_kwargs = {'ms': 5, 'marker': 'o', 'ls': '', 'alpha': 0.75}
+    ax_z.plot(x_prof_z, prof_z, c='C1', label='Z', **plot_kwargs)
+    ax_y.plot(x_prof_y, prof_y, c='C0', label='Y', **plot_kwargs)
+    ax_x.plot(x_prof_x, prof_x, c='C2', label='X', **plot_kwargs)
+
+    # Plot 1D PSF fits
+    ax_z.plot(x_uz, gaussian_1D(u, *popt_z), 'k-',
+              label=f'{popt_z[1]:.2f}nm\nFWHM')
+    ax_y.plot(x_uy, gaussian_1D(u, *popt_y), 'k-',
+              label=f'{popt_y[1]:.2f}nm\nFWHM')
+    ax_x.plot(x_ux, gaussian_1D(u, *popt_x), 'k-',
+              label=f'{popt_x[1]:.2f}nm\nFWHM')
+
+    # --- Aesthetics ---
+    # XY projection
+    ax_xy.text(0.02, 0.02, 'XY', fontsize=14, color='white', transform=ax_xy.transAxes)
+    ax_xy.set_xlabel('X (μm)')
+    ax_xy.set_ylabel('Y (μm)')
+    ax_xy.xaxis.set_ticks_position('top')
+    ax_xy.xaxis.set_label_position('top')
+    # YZ projection
+    ax_yz.text(0.02, 0.02, 'YZ', fontsize=14, color='white', transform=ax_yz.transAxes)
+    ax_yz.set_xlabel('Z (μm)')
+    ax_yz.set_ylabel('Y (μm)')
+    ax_yz.xaxis.set_ticks_position('top')
+    ax_yz.xaxis.set_label_position('top')
+    ax_yz.yaxis.set_ticks_position('right')
+    ax_yz.yaxis.set_label_position('right')
+    # XZ projection
+    ax_xz.text(0.02, 0.02, 'XZ', fontsize=14, color='white', transform=ax_xz.transAxes)
+    ax_xz.set_xlabel('X (μm)')
+    ax_xz.set_ylabel('Z (μm)')
+    # Other stuff
+    [ax.legend(loc='upper right') for ax in [ax_z, ax_y, ax_x]]
+    [ax.grid(ls=':') for ax in [ax_z, ax_y, ax_x]]
+    plt.subplots_adjust(hspace=0.5, wspace=0.5)
+
+
+def plot_psfs(psfs):
+    """Plot MIPs of extracted PSFs"""
+    # Create figure
+    ncols = 8
+    nrows = int(np.ceil(len(psfs) / ncols))
+    fig, axes = plt.subplots(ncols=ncols, nrows=nrows,
+                             figsize=(4*ncols, 4*nrows))
+    # Loop through PSFs
+    for i, psf in enumerate(psfs):
+        ax = axes.flat[i]
+        mip = np.max(psf, axis=0)
+        ax.imshow(mip, cmap=fire)
+        ax.set_title(f'PSF {i}')
+    # Remove empty subplots
+    [fig.delaxes(axes.flat[-i-1]) for i in range(ncols*nrows - len(psfs))]
+    return fig
 
 
 def plot_psf_localizations(df):
