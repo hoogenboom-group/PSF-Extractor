5b39276a4bb867e36b0b27703be5dfb7821a2920
lanery <ryanilane@gmail.com>
2022-05-06 17:06:38 +0200

    get version from setup.py
    Former-commit-id: 3e576d5f2007868b64377b6777a235ceaf64f23e

--- psf_extractor/__init__.py
+++ psf_extractor/__init__.py
@@ -1,9 +1,12 @@
-from . import extractor
-from . import gauss
+from pkg_resources import get_distribution
+
 from . import parallel
 
+from .extractor import *
+from .gauss import *
 from .plotting import *
 from .util import *
 
-__version__ = '0.2'
-__all__ = ['extractor', 'gauss', 'parallel']
+
+__version__ = get_distribution('PSF-Extractor').version
+__all__ = ['parallel']
