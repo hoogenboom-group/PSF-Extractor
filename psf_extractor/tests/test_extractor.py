5a066599e04c13571c5a095e8142b2dfce2b2882
lanery <ryanilane@gmail.com>
2022-05-19 17:18:26 +0200

    Renamed Yoram's data
    Former-commit-id: e1c81585752255df15aa04514bde3f62f777510b

--- psf_extractor/tests/test_extractor.py
+++ psf_extractor/tests/test_extractor.py
@@ -6,22 +6,6 @@ from ..util import generate_image
 
 class TestStack:
 
-    def test_list_png(self):
-        # List of png files from data module
-        stem = '20201130_162150_collar_0.146_yaw_1.62deg_cyls_corr_back_foc_325.3um_15deg'
-        file_pattern = [
-            f'./data/sample_zstack_png_sequence/{stem}0000.png',
-            f'./data/sample_zstack_png_sequence/{stem}0001.png',
-            f'./data/sample_zstack_png_sequence/{stem}0002.png',
-            f'./data/sample_zstack_png_sequence/{stem}0003.png',
-            f'./data/sample_zstack_png_sequence/{stem}0004.png',
-            f'./data/sample_zstack_png_sequence/{stem}0005.png',
-        ]
-        # Create stack
-        stack = load_stack(file_pattern)
-        # Known shape
-        assert stack.shape == (6, 2048, 2448)
-
     def test_list_tif(self):
         # List of tif files from data module
         file_pattern = [
@@ -37,30 +21,13 @@ class TestStack:
         # Known shape
         assert stack.shape == (6, 1749, 2034)
 
-    def test_directory_png(self):
-        # Path to png stack directory
-        file_pattern = './data/sample_zstack_png_sequence/'
-        # Create stack
-        stack = load_stack(file_pattern)
-        # Known shape
-        assert stack.shape == (32, 2048, 2448)
-
     def test_directory_tif(self):
         # Path to png stack directory
-        file_pattern = './data/sample_zstack_tif_sequence/'
+        file_pattern = './data/sample_zstack/'
         # Create stack
         stack = load_stack(file_pattern)
         # Known shape
-        assert stack.shape == (23, 1749, 2034)
-
-    def test_tif_stack(self):
-        # Path to tif stack in data module
-        file_pattern = './data/sample_zstack_single_tif/20201130_162150_collar' +\
-                       '_0.146_yaw_1.62deg_cyls_corr_back_foc_325.3um_15deg.tif'
-        # Create stack
-        stack = load_stack(file_pattern)
-        # Known shape
-        assert stack.shape == (23, 1749, 2034)
+        assert stack.shape == ()
 
 
 class TestMassFiltering:
